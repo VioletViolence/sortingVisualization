e735a729328eff361f365fc6e9995757ab9e762a
VioletViolence <brosinskiy@gmail.com>
2019-12-19 14:38:39 +0200

    Add visualization files

---
+++ src/CountingSort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class CountingSort {
+
+    static int[] countingSort(int[] arr){
+
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,6,2,6,8,1,8,324,12,7,3,2,2,2,6,68,12,4,46,1};
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(countingSort(array)));
+    }
+}
