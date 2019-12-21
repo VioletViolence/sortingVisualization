73e7433e060342422ded3efd005d48e83f851c81
VioletViolence <brosinskiy@gmail.com>
2019-12-22 00:21:56 +0200

    Shorthanded counting sort

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -4,14 +4,26 @@ public class CountingSort {
 
     static int[] countingSort(int[] arr){
 
-
         // TODO: Fing array's mox number
-
-
+            int max = 0;
+        for (int number: arr) {
+            if(number > max) max = number;
+        }
         // TODO: Count number or mentions of numbers
+        int[] countArray = new int[max+1];
+        for (int number: arr) {
+            countArray[number]++;
+        }
 
-
-        // TODO: Arrange an array from the bottom up 
+        // TODO: Arrange an array from the bottom up
+        int finalCounter = 0;
+        for (int j = 0; j < countArray.length; j++) {
+            while(countArray[j]>0){
+                arr[finalCounter] = j;
+                countArray[j]--;
+                finalCounter++;
+            }
+        }
 
         return arr;
     }
