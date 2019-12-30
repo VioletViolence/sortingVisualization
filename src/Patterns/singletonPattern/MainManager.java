ec862becd03f259a7ff222c9751a5f2f682d024e
VioletViolence <brosinskiy@gmail.com>
2019-12-30 18:57:23 +0200

    Singleton finished

--- src/Patterns/singletonPattern/MainManager.java
+++ src/Patterns/singletonPattern/MainManager.java
@@ -1,4 +1,15 @@
 package Patterns.singletonPattern;
 
 public class MainManager {
+    public static void main(String[] args) {
+        SingletonClass singletonInstance = SingletonClass.getInstance();
+
+        SingletonClass attemptOfSecondInstance = SingletonClass.getInstance();
+
+        System.out.println(singletonInstance.getFullTimeOfCreation());
+        System.out.println(attemptOfSecondInstance.getFullTimeOfCreation());
+        if(singletonInstance.getFullTimeOfCreation() == attemptOfSecondInstance.getFullTimeOfCreation()) {
+            System.out.println("Since both times are the same we have a working proof that singleton pattern was implemented correctly");
+        }else System.out.println("Something is seriously wrong");
+    }
 }
