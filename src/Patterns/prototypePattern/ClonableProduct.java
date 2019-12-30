7ca2a0dd96a4569d5015c3e6bcbccd92a5186a9f
VioletViolence <brosinskiy@gmail.com>
2019-12-30 19:49:02 +0200

    Prototype Prototype

---
+++ src/Patterns/prototypePattern/ClonableProduct.java
@@ -0,0 +1,12 @@
+package Patterns.prototypePattern;
+
+import java.time.LocalDateTime;
+
+public class ClonableProduct implements Cloneable {
+    private String name;
+    private LocalDateTime timeOfCreation;
+
+    public ClonableProduct(){
+
+    }
+}
---
+++ src/Patterns/prototypePattern/Main.java
@@ -0,0 +1,7 @@
+package Patterns.prototypePattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
