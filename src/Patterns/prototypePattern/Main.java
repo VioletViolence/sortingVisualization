675f8b3d38055dc9dee09ea2615581763d7affb1
VioletViolence <brosinskiy@gmail.com>
2019-12-31 03:38:18 +0200

    Prototype Prototype

--- src/Patterns/prototypePattern/Main.java
+++ src/Patterns/prototypePattern/Main.java
@@ -2,6 +2,7 @@ package Patterns.prototypePattern;
 
 public class Main {
     public static void main(String[] args) {
-
+        ClonableProduct clonableProduct = new ClonableProduct("NewNew");
+        ClonableProduct secondClone = clonableProduct.clone();
     }
 }
